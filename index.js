09fc365bd9f787d0745715bc1799cbd64e5dc5db
liuliqianxiao <liuliqianxiao@126.com>
2021-08-09 21:05:24 +0800

    xxx

--- index.js
+++ index.js
@@ -4,34 +4,23 @@ const https = require('https');
 const {URL} = require('url');
 const fileUtils = require("./src/fileUtils");
 
+//资源版本记录文件
+var versionFileName = "compress.json";
+var oldVersionDic;
+var currentVersionDic;
 //未压缩的原始资源目录
-var originalResourceRoot = "";
-//压缩资源的输出目录
-var destResourceRoot = ""
-
-let argLength = process.argv.length;
-if (argLength === 2) {
-    // originalResourceRoot = path.resolve("./test");
-    originalResourceRoot = path.resolve("C:\\Users\\05\\Desktop\\test");
-    destResourceRoot = path.join(originalResourceRoot, "output");
-} else if (argLength === 3) {
-    originalResourceRoot = path.resolve(process.argv[2]);
-    destResourceRoot = path.join(originalResourceRoot, "output");
-    console.warn("未设置资源输出目录，默认为" + destResourceRoot)
-} else {
-    originalResourceRoot = path.resolve(process.argv[2]);
-    destResourceRoot = path.resolve(process.argv[3]);
-    console.log(`当前原始资源目录：${originalResourceRoot},压缩输出目录：${destResourceRoot}`)
-}
-
-//如果有输出目录，先清空，否则就创建输出目录
-fileUtils.rmdirSync(destResourceRoot, false);
-fileUtils.mkdirSync(destResourceRoot);
-
-const maxHttpThread = 10;//最多同时有多少个http请求
-var currentUseHttp = 0;//当前使用的http个数
+var originalResourceRoot = "../../bin";
+//压缩资源临时目录
+var destResourceRoot;
+//最多同时有多少个http请求
+const maxHttpThread = 10;
+//当前使用的http个数
+var currentUseHttp = 0;
+//最大失败次数
 var maxTryTimes = 3;
+//当前重试次数
 var currentTryTime = 0;
+//关注的资源后缀名
 const watchExtension = ['.jpg', '.png'];
 const options = {
     method: 'POST',
@@ -46,27 +35,68 @@ const options = {
             'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36'
     }
 };
-
+//是否用压缩文件覆盖原文件
+var overWriteOriginFiles = false;
 var waitToCompressFiles = [];
 var failCompressFiles = [];
 
-//扫描目录下所有可以压缩的图片资源
-filterImagesFiles(originalResourceRoot);
-console.log(`>>>>>>>>>>>>>>>>>>>>>>>>总计有${waitToCompressFiles.length}个文件等待压缩<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<`);
-
+parseArgs();
+findNewFiles();
 startUpload();
 
-// 获取文件列表
-function filterImagesFiles(imagePathOrDir) {
-    if (fs.statSync(imagePathOrDir).isDirectory()) {
-        var fileList = fs.readdirSync(imagePathOrDir);
-        for (let i = 0; i < fileList.length; i++) {
-            filterImagesFiles(path.join(imagePathOrDir, fileList[i]));
-        }
+function parseArgs() {
+    let argLength = process.argv.length;
+    if (argLength === 3) {
+        originalResourceRoot = path.resolve(process.argv[2]);
+    } else if (argLength !== 2) {
+        throw new Error("未知的参数！")
+    }
+    originalResourceRoot = originalResourceRoot.replace(/\\/g, "/");
+    if (originalResourceRoot.lastIndexOf("/") !== originalResourceRoot.length - 1) {
+        originalResourceRoot += "/";
+    }
+    destResourceRoot = path.join(path.dirname(originalResourceRoot), "output").replace(/\\/g, "/") + "/";
+
+    console.log(`当前原始资源目录：${originalResourceRoot},压缩输出目录：${destResourceRoot}`)
+
+    //如果有输出目录，先清空，否则就创建输出目录
+    if (fs.existsSync(destResourceRoot)) {
+        fileUtils.rmdirSync(destResourceRoot, false);
     } else {
-        var extension = path.extname(imagePathOrDir);
-        if (watchExtension.indexOf(extension) !== -1) {
-            waitToCompressFiles.push(imagePathOrDir);
+        fileUtils.mkdirSync(destResourceRoot);
+    }
+}
+
+function findNewFiles() {
+    var versionFullPath = path.join(originalResourceRoot, versionFileName);
+    if (fs.existsSync(versionFullPath)) {
+        oldVersionDic = JSON.parse(fs.readFileSync(versionFullPath, "utf-8"));
+    } else {
+        oldVersionDic = {};
+    }
+    currentVersionDic = {};
+    //扫描目录下所有可以压缩的图片资源
+    filterImagesFiles(originalResourceRoot);
+    console.log(`>>>>>>>>>>>>>>>>>>>>>>>>总计有${waitToCompressFiles.length}个文件等待压缩<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<`);
+
+    // 获取文件列表
+    function filterImagesFiles(imagePathOrDir) {
+        if (fs.statSync(imagePathOrDir).isDirectory()) {
+            var fileList = fs.readdirSync(imagePathOrDir);
+            for (let i = 0; i < fileList.length; i++) {
+                filterImagesFiles(path.join(imagePathOrDir, fileList[i]));
+            }
+        } else {
+            var extension = path.extname(imagePathOrDir);
+            if (watchExtension.indexOf(extension) !== -1) {
+                imagePathOrDir = imagePathOrDir.replace(/\\/g, "/");
+                let shortName = imagePathOrDir.replace(originalResourceRoot, "");
+                let hashStr = fileUtils.generateFileMd5(imagePathOrDir);
+                currentVersionDic[shortName] = hashStr;
+                if (hashStr !== oldVersionDic[shortName]) {
+                    waitToCompressFiles.push(imagePathOrDir);
+                }
+            }
         }
     }
 }
@@ -156,9 +186,21 @@ function fileDownload(imagePath, obj) {
 }
 
 function saveCompressedFile(originalPath, compressContent, inputSize, outputSize, compressRatio) {
-    let outputImagePath = path.join(destResourceRoot, originalPath.replace(originalResourceRoot + path.sep, ""));
+    originalPath = originalPath.replace(/\\/g, "/");
+    let outputImagePath;
+    if (overWriteOriginFiles) {
+        outputImagePath = originalPath;
+    } else {
+        outputImagePath = path.join(destResourceRoot, originalPath.replace(originalResourceRoot, ""));
+    }
     fileUtils.mkdirSync(path.dirname(outputImagePath));
     fs.writeFileSync(outputImagePath, compressContent, 'binary');
+
+    if (overWriteOriginFiles) {
+        var shortName = originalPath.replace(originalResourceRoot, "/");
+        currentVersionDic[shortName] = fileUtils.generateFileMd5(outputImagePath);
+    }
+
     console.log(`[====================${originalPath}] \n 压缩成功，原始大小:${inputSize}，压缩大小:${outputSize}，优化比例:${compressRatio}`);
 }
 
@@ -174,6 +216,15 @@ function compressAllComplete() {
         console.log(`===========================所有资源压缩完成,失败${failCompressFiles.length}个==================================`);
         failCompressFiles.forEach((imagePath) => {
             console.log(`XXXXXXXXXXXXXXXXXXXXXXXXX    ${imagePath}    XXXXXXXXXXXXXXXXXXXXXXXXX`);
+            var shortName = imagePath.replace(/\\/g, "/").replace(originalResourceRoot, "");
+            currentVersionDic[shortName] = null;
+            delete currentVersionDic[shortName];
         });
+        copyAndSave();
     }
+}
+
+function copyAndSave() {
+    var versionFilePath = path.join(originalResourceRoot, versionFileName);
+    fs.writeFileSync(versionFilePath, JSON.stringify(currentVersionDic, null, "\t"), "utf-8");
 }
